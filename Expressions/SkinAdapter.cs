7ecc9722fe45b2547d6e57c99ab0e7bfd004cd7f
Siim Raudsepp <siimraudsepp@msn.com>
2026-04-21 20:34:47 +0300

    Fix default face expression selection on first install

--- Expressions/SkinAdapter.cs
+++ Expressions/SkinAdapter.cs
@@ -7,11 +7,9 @@ namespace Expressions;
 
 internal sealed class SkinAdapter
 {
-    private readonly EntityBehaviorExtraSkinnable? _vanilla;
-    private readonly PlayerModelLib.PlayerSkinBehavior? _pml;
+    private readonly EntityBehaviorExtraSkinnable _skin;
 
-    private SkinAdapter(EntityBehaviorExtraSkinnable vanilla) => _vanilla = vanilla;
-    private SkinAdapter(PlayerModelLib.PlayerSkinBehavior pml) => _pml = pml;
+    private SkinAdapter(EntityBehaviorExtraSkinnable skin) => _skin = skin;
 
     public static SkinAdapter? Get(Entity entity)
     {
@@ -22,24 +20,16 @@ internal sealed class SkinAdapter
         return null;
     }
 
-    public IEnumerable<SkinnablePart> AvailableSkinParts =>
-        _pml != null ? (IEnumerable<SkinnablePart>)_pml.AvailableSkinParts.Get() : _vanilla!.AvailableSkinParts;
+    public IEnumerable<SkinnablePart> AvailableSkinParts => _skin.AvailableSkinParts;
 
-    public IEnumerable<AppliedSkinnablePartVariant> AppliedSkinParts =>
-        _pml != null ? (IEnumerable<AppliedSkinnablePartVariant>)_pml.AppliedSkinParts.Get() : _vanilla!.AppliedSkinParts;
+    public IEnumerable<AppliedSkinnablePartVariant> AppliedSkinParts => _skin.AppliedSkinParts;
 
     public SkinnablePart? GetPart(string code)
     {
-        if (_pml != null) return _pml.AvailableSkinPartsByCode.GetValue(code);
-        _vanilla!.AvailableSkinPartsByCode.TryGetValue(code, out var part);
+        _skin.AvailableSkinPartsByCode.TryGetValue(code, out var part);
         return part;
     }
 
-    public void SelectSkinPart(string partCode, string variantCode)
-    {
-        if (_pml != null)
-            _pml.SelectSkinPart(partCode, variantCode);
-        else
-            _vanilla!.selectSkinPart(partCode, variantCode);
-    }
+    public void SelectSkinPart(string partCode, string variantCode) =>
+        _skin.selectSkinPart(partCode, variantCode);
 }
